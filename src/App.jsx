bde7d1f301fb2546848b9c38a51b98783c73a578
Elias <elias@st-innovations.net>
2025-05-05 14:01:55 +0200

    solved code conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import StartPage from "./pages/StartPage";
 import EndPage from "./pages/EndPage";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
-
+import AnimalCard from "./components/AnimalCard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
-      <Route path="/alternative" element={<AnimalCard/>} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/animalcards" element={<AnimalCard />} />
       <Route path="/end" element={<EndPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import AnimalCard from "../styles/AnimalCard.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../styles/AnimalCard.css";
 
-
+import bird from "../assets/gamepictures/bird.jpg";
+import dog from "../assets/gamepictures/dog.jpg";
+import cat from "../assets/gamepictures/cat.jpg";
 
 const AnimalCard = () => {
   return (
-    <div className='option-wrapper'>
-    <div className='bacground-ellipse top-left'/>
-    <div className='background-ellipse bottom-right'/>
-        <h2>FÅGEL</h2>
-        <div className="image-grid">
+    <div className="option-wrapper">
+      <div className="bacground-ellipse top-left" />
+      <div className="background-ellipse bottom-right" />
+      <h2>FÅGEL</h2>
+      <div className="image-grid">
         {[bird, dog, cat].map((img, i) => (
           <div className="image-slot" key={i}>
-            <img src={img} alt={`option-${i}`} />
+            <img src={img.default ?? img} alt={`option-${i}`} />
           </div>
         ))}
       </div>
-            <button className='next-button'> Nästa</button>
-
-      
-
-  
-
+      <button className="next-button"> Nästa</button>
     </div>
-  )
-}
+  );
+};
 
-export default AnimalCard
+export default AnimalCard;

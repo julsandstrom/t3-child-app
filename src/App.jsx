5ead9c3b222726e6828f4bba366d97cf037d0683
Billieliud24 <marryahferom1@gmail.com>
2025-05-05 10:23:20 +0200

    uppdatera befentlipg komponent med quiz-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./styles/App.css";
-
+import { BrowserRouter as Router, Routes ,Route} from "react-router-dom";
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/alternative" element={<AnimalCard/>}></Route>
+        </Routes>
+      </Router>
       <div>
         <h1>Team 3</h1>
       </div>

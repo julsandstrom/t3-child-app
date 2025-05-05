3b85e3d66ef638de24c5731237775138c0bf93b1
KarezP <karezpeshawa75@gmail.com>
2025-05-05 12:29:30 +0200

    Lägger till 404-sida med djungelbakgrund och mobilvänlig design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./styles/App.css";
 import EndPage from './components/EndPage';
+import NotFound from './components/NotFound';
+import { Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <>
-    <EndPage />
-      <div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<EndPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 

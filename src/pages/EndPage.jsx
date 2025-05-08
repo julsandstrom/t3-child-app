6e15d1b7dda5eb7320cd3ba8928ea8a91e0d6f73
Elias <elias@st-innovations.net>
2025-05-08 15:11:23 +0200

    fixed import of endpage.css

--- src/pages/EndPage.jsx
+++ src/pages/EndPage.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Confetti from 'react-confetti';
-import djurikoner from '../assets/djurikoner.png';
-import './EndPage.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Confetti from "react-confetti";
+import djurikoner from "../assets/djurikoner.png";
+import "../styles/EndPage.css";
+import { useNavigate } from "react-router-dom";
 
 const EndPage = () => {
-     const navigate = useNavigate();
-    return (
-        <div className='end-page'>
-        <Confetti className='confetti' /> 
-            
-        <div className="text-wrapper">
-            <h1>Bra jobbat!</h1>
-            <p>Du klarade det!</p> 
-        </div>
+  const navigate = useNavigate();
+  return (
+    <div className="end-page">
+      <Confetti className="confetti" />
 
-        <button className="back-button" onClick={() => navigate('/')}>
+      <div className="text-wrapper">
+        <h1>Bra jobbat!</h1>
+        <p>Du klarade det!</p>
+      </div>
+
+      <button className="back-button" onClick={() => navigate("/")}>
         Gå tillbaka
-        </button>
+      </button>
 
-        <div className="djur-ikoner">
-            <img src={djurikoner} alt="djurikoner" />
-        </div>
+      <div className="djur-ikoner">
+        <img src={djurikoner} alt="djurikoner" />
+      </div>
     </div>
-    );
+  );
 };
 
 export default EndPage;

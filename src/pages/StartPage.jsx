c3349f9553827c1615685cc55096094b319d2f98
Julian Sandström <juliiansandstrom@gmail.com>
2025-05-08 11:45:49 +0200

    Added audio for animals on start page

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -4,22 +4,42 @@ import StartSign from "../assets/startpage/Sign-start.png";
 import hangingMonkey from "../assets/startpage/monkey-hanging.png";
 import cornerZebra from "../assets/startpage/zebra-corner.png";
 import cornerParrot from "../assets/startpage/side-parrot.png";
+import monkeySound from "../assets/startpage/monkey.mp3";
+import zebraSound from "../assets/startpage/zebra.mp3";
+import parrotSound from "../assets/startpage/parrot.mp3";
 import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
   const navigate = useNavigate();
 
+  const playSound = (soundFile) => {
+    const audio = new Audio(soundFile);
+    audio.play();
+  };
+
   return (
     <>
       <div className="start-page">
         <div className="hanging-monkey">
-          <img src={hangingMonkey} alt="Hängande apa" />
+          <img
+            src={hangingMonkey}
+            alt="Hängande apa"
+            onClick={() => playSound(monkeySound)}
+          />
         </div>
         <div className="corner-zebra">
-          <img src={cornerZebra} alt="Glad Zebra" />
+          <img
+            src={cornerZebra}
+            alt="Glad Zebra"
+            onClick={() => playSound(zebraSound)}
+          />
         </div>
         <div className="corner-parrot">
-          <img src={cornerParrot} alt="Glad Papegoja" />
+          <img
+            src={cornerParrot}
+            alt="Glad Papegoja"
+            onClick={() => playSound(parrotSound)}
+          />
         </div>
         <div className="game-name">
           <img src={lillaDjurVarlden} alt="Lilla Djurvärlden" />

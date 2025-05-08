a7e14be7bd7d616b613eb689c9d1ca2247f3728c
KarezP <karezpeshawa75@gmail.com>
2025-05-08 12:09:06 +0200

    Lägg till knapp till endpage + uppdaterad CSS

--- src/components/EndPage.jsx
+++ src/components/EndPage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Confetti from 'react-confetti';
 import djurikoner from '../assets/djurikoner.png';
 import './EndPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const EndPage = () => {
+     const navigate = useNavigate();
     return (
         <div className='end-page'>
         <Confetti className='confetti' /> 
@@ -12,6 +14,10 @@ const EndPage = () => {
             <h1>Bra jobbat!</h1>
             <p>Du klarade det!</p> 
         </div>
+
+        <button className="back-button" onClick={() => navigate('/')}>
+        Gå tillbaka
+        </button>
 
         <div className="djur-ikoner">
             <img src={djurikoner} alt="djurikoner" />

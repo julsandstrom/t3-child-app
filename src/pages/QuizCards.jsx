81d32ec7643cb5e2e3702607d782906a36e4c1bc
Yusra Aman <Yusra.aman00@gmail.com>
2025-05-09 16:35:06 +0200

    Uppdaterad design på quiz cards

--- src/pages/QuizCards.jsx
+++ src/pages/QuizCards.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import bird from "../assets/gamepictures/bird.jpg";
 import dog from "../assets/gamepictures/dog.jpg";
-import cat from "../assets/gamepictures/cat.jpg"; 
-import '../styles/QuizCards.css';
+import cat from "../assets/gamepictures/cat.jpg";
+import "../styles/QuizCards.css";
 
 const QuizCards = () => {
   const navigate = useNavigate();
 
-  // När man klickar på rätt svar (hunden)
   const handleCorrectClick = () => {
     navigate("/endpage");
   };
 
-return (
+  return (
     <div className="quiz-cards">
-    <div className="text-wrapper">
-      <h1>Hund</h1>
+      <div className="text-wrapper">
+        <h1>Hund</h1>
+      </div>
+
+      <div className="cards-container">
+        <div className="wrong">
+          <div className="wooden-sign">
+            <img src={bird} alt="bird" />
+          </div>
+        </div>
+
+        <div className="correct" onClick={handleCorrectClick}>
+          <div className="wooden-sign">
+            <img src={dog} alt="dog" />
+          </div>
+        </div>
+
+        <div className="wrong">
+          <div className="wooden-sign">
+            <img src={cat} alt="cat" />
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div className="cards-container">
-      <div className="wrong">
-        <img src={bird} alt="bird" />
-      </div>
-
-      <div className="correct" onClick={handleCorrectClick}> 
-        <img src={dog} alt="dog"/>
-      </div>
-
-      <div className="wrong">
-        <img src={cat} alt="cat" />
-      </div>
-    </div>
-  </div>
-);
-}
-        {/* <div className="cards-container">
+  );
+};
+{
+  /* <div className="cards-container">
             <div className='first-card'>
                 <img src={bird} alt='bird'></img>
             </div>
@@ -47,8 +53,7 @@ return (
             <div className='third-card'>
                 <img src={cat} alt='cat'></img>
             </div>
-        </div> */}
-    
-
+        </div> */
+}
 
 export default QuizCards;

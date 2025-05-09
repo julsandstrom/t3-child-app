fc3628d80b366e62b237f435ea2e0cdf252ab02c
KarezP <karezpeshawa75@gmail.com>
2025-05-09 09:44:12 +0200

    EndPage: Animerad skylt och responsiv design

--- src/pages/EndPage.jsx
+++ src/pages/EndPage.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import Confetti from "react-confetti";
-import djurikoner from "../assets/djurikoner.png";
+import spelaIgen from "../assets/startpage/spelaIgen.png";
+import braJobbat from "../assets/startpage/braJobbat.png";
+
 import "../styles/EndPage.css";
 import { useNavigate } from "react-router-dom";
 
 const EndPage = () => {
   const navigate = useNavigate();
+
   return (
     <div className="end-page">
       <Confetti className="confetti" />
 
+      <div className="hanging-monkey">
+        <img src="/src/assets/startpage/monkey-hanging.png" alt="apa" />
+      </div>
+      <div className="corner-zebra">
+        <img src="/src/assets/startpage/zebra-corner.png" alt="zebra" />
+      </div>
+      <div className="corner-parrot">
+        <img src="/src/assets/startpage/side-parrot.png" alt="papegoja" />
+      </div>
+
       <div className="text-wrapper">
-        <h1>Bra jobbat!
-            <br/>
-          Du klarade det!
-        </h1>
-      
+        <div className="bra-jobbat-animation">
+          <img src={braJobbat} alt="Bra jobbat - Du klarade det!" />
+        </div>
       </div>
 
-      <button className="spela-igen" onClick={() => navigate("/")}>
-       Spela igen
+      <button className="play-again-button" onClick={() => navigate("/")}>
+        <img src={spelaIgen} alt="Spela igen" />
       </button>
-
-      <div className="djur-ikoner">
-        <img src={djurikoner} alt="djurikoner" />
-      </div>
     </div>
   );
 };

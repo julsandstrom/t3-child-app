3cd2349a2ab9f5ee2cbba2a46fd85366426e0cee
Elias <elias@st-innovations.net>
2025-05-12 13:18:28 +0200

    changed some of the pictures for the quiz to fit the theme

--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -1,29 +1,29 @@
 import björn from "../images-json/bjorn.png";
 import elefant from "../images-json/elefant.webp";
-import fisk from "../images-json/fisk.webp";
+import fisk from "../images-json/fisk.png";
 import get from "../images-json/get.webp";
 import giraff from "../images-json/giraff.webp";
-import hund from "../images-json/hund.webp";
+import hund from "../images-json/hund.png";
 import kamel from "../images-json/Kamel.webp";
 import kanin from "../images-json/kanin.png";
 import katt from "../images-json/katt.png";
 import ko from "../images-json/ko.png";
 import lejon from "../images-json/lion.webp";
-import mus from "../images-json/mus.webp";
-import räv from "../images-json/räv.webp";
+import mus from "../images-json/mus.png";
+import räv from "../images-json/rav.png";
 
 export const imageMap = {
     "bjorn.png": björn,
     "elefant.webp": elefant,
-    "fisk.webp": fisk,
+    "fisk.png": fisk,
     "get.webp": get,
     "giraff.webp": giraff,
-    "hund.webp": hund,
+    "hund.png": hund,
     "Kamel.webp": kamel,
     "kanin.png": kanin,
     "katt.png": katt,
     "ko.png": ko,
     "lion.webp": lejon,
-    "mus.webp": mus,
-    "räv.webp": räv,
+    "mus.png": mus,
+    "rav.png": räv,
 };

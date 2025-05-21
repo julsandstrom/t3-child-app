3a256675966424c352cb327ff30c8f53e2fc972a
Julian Sandström <juliiansandstrom@gmail.com>
2025-05-21 13:19:05 +0200

    added rotate info

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,39 @@ import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 import QuizCards from "./pages/QuizCards";
 import BakgrundsLjud from "./components/BakgrundsLjud";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLandscape, setIsLandscape] = useState(
+    window.innerWidth > window.innerHeight
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLandscape(window.innerWidth > window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!isLandscape) {
+    return (
+      <div className="rotate-wrapper">
+        <p className="info-text">Vänligen rotera din enhet.</p>
+      </div>
+    );
+  }
+
   return (
     <>
-    <BakgrundsLjud />
-    <Routes>
-      <Route path="/" element={<StartPage />} />
-      <Route path="/quizcards" element={<QuizCards />} />
-      <Route path="/endpage" element={<EndPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+      <BakgrundsLjud />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/quizcards" element={<QuizCards />} />
+        <Route path="/endpage" element={<EndPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
